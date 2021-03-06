54708b3edf0b1ec76dd463f47d89597008d9c649
Likh-Alex <likhachov991@protonmail.com>
2021-03-06 17:03:11 +0200

    initial commit

---
+++ src/test/java/payment_system/dev/com/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package payment_system.dev.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
